d58ceb09e717edb8074efd18219ec5a844ce12fd
Dave LeCompte <tsmaster@gmail.com>
2023-08-04 17:15:39 -0700

    Part 1 - showing the @, moving around the screen

---
+++ src/main.rs
@@ -0,0 +1,142 @@
+// src/main.rs
+
+use coord_2d::{Coord, Size};
+use direction::CardinalDirection;
+use rgb24::Rgb24;
+
+fn main() {
+    use chargrid_graphical::{Config, Context, Dimensions, FontBytes};
+    const CELL_SIZE_PX: f64 = 24.0;
+    let context = Context::new(Config {
+        font_bytes: FontBytes {
+            normal: include_bytes!("./fonts/PxPlus_IBM_CGAthin.ttf").to_vec(),
+            bold: include_bytes!("./fonts/PxPlus_IBM_CGA.ttf").to_vec(),
+        },
+        title: "Chargrid Tutorial".to_string(),
+        window_dimensions_px: Dimensions {
+            width: 960.0,
+            height: 720.0,
+        },
+        cell_dimensions_px: Dimensions {
+            width: CELL_SIZE_PX,
+            height: CELL_SIZE_PX,
+        },
+        font_scale: Dimensions {
+            width: CELL_SIZE_PX,
+            height: CELL_SIZE_PX,
+        },
+        underline_width_cell_ratio: 0.1,
+        underline_top_offset_cell_ratio: 0.8,
+        resizable: false,
+    });
+    let screen_size = Size::new(40, 30);
+    let app = App::new(screen_size);
+    context.run_app(app);        
+}
+
+struct App {
+    data: AppData,
+    view: AppView,
+}
+
+impl App {
+    fn new(screen_size: Size) -> Self {
+        Self {
+            data: AppData::new(screen_size),
+            view: AppView::new(),
+        }
+    }
+}
+
+impl chargrid::app::App for App {
+    fn on_input (
+        &mut self,
+        input: chargrid::app::Input,
+    ) -> Option<chargrid::app::ControlFlow> {
+        use chargrid::input::{keys, Input};
+        match input {
+            Input::Keyboard(keys::ETX) | Input::Keyboard(keys::ESCAPE) => {
+                Some(chargrid::app::ControlFlow::Exit)
+            }
+            other => {
+                self.data.handle_input(other);
+                None
+            }
+        }
+    }
+
+    fn on_frame<F, C> (
+        &mut self,
+        _since_last_frame: chargrid::app::Duration,
+        view_context: chargrid::app::ViewContext<C>,
+        frame: &mut F,
+    ) -> Option<chargrid::app::ControlFlow>
+    where
+        F: chargrid::app::Frame,
+        C: chargrid::app::ColModify,
+    {
+        use chargrid::render::View;
+        self.view.view(&self.data, view_context, frame);
+        None
+    }
+}
+
+
+
+struct AppData {
+    screen_size: Size,
+    player_coord: Coord,
+}
+
+impl AppData {
+    fn new(screen_size: Size) -> Self {
+        Self {
+            screen_size,
+            player_coord: screen_size.to_coord().unwrap() / 2,
+        }
+    }
+
+    fn maybe_move_player(&mut self, direction: CardinalDirection) {
+        let new_player_coord = self.player_coord + direction.coord();
+        if new_player_coord.is_valid(self.screen_size) {
+            self.player_coord = new_player_coord;
+        }
+    }
+
+    fn handle_input(&mut self, input: chargrid::input::Input) {
+        use chargrid::input::{Input, KeyboardInput};
+        match input {
+            Input::Keyboard(key) => match key {
+                KeyboardInput::Left => self.maybe_move_player(CardinalDirection::West),
+                KeyboardInput::Right => self.maybe_move_player(CardinalDirection::East),
+                KeyboardInput::Up => self.maybe_move_player(CardinalDirection::North),
+                KeyboardInput::Down => self.maybe_move_player(CardinalDirection::South),
+                _ => (),
+            },
+            _ => (),
+        }
+    }
+}
+
+struct AppView {}
+
+impl AppView {
+    fn new() -> Self {
+        Self {}
+    }
+}
+
+impl <'a> chargrid::render::View<&'a AppData> for AppView {
+    fn view<F: chargrid::app::Frame, C: chargrid::app::ColModify>(
+        &mut self,
+        data: &'a AppData,
+        context: chargrid::app::ViewContext<C>,
+        frame: &mut F,
+    ) {
+        let view_cell = chargrid::render::ViewCell::new()
+            .with_character('@')
+            .with_foreground(Rgb24::new_grey(255));
+        frame.set_cell_relative(data.player_coord, 0, view_cell, context);
+    }
+}
+
