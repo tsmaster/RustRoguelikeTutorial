9067f4b4a740246ae50678e4ee60d3139e65b3b0
Dave LeCompte <tsmaster@gmail.com>
2023-08-04 17:33:13 -0700

    Part 2 - refactor

---
+++ src/app.rs
@@ -0,0 +1,111 @@
+// app.rs
+
+use crate::game::GameState;
+use chargrid::{
+    app::{App as ChargridApp, ControlFlow},
+    input::{keys, Input, KeyboardInput},
+    render::{ColModify, Frame, View, ViewCell, ViewContext},
+};
+use coord_2d::Size;
+use direction::CardinalDirection;
+use rgb24::Rgb24;
+use std::time::Duration;
+
+
+struct AppData {
+    game_state: GameState,
+}
+
+impl AppData {
+    fn new(screen_size: Size) -> Self {
+        Self {
+            game_state: GameState::new(screen_size),
+        }
+    }
+
+    fn handle_input(&mut self, input: chargrid::input::Input) {
+        use chargrid::input::{Input, KeyboardInput};
+        match input {
+            Input::Keyboard(key) => match key {
+                KeyboardInput::Left => game_state.maybe_move_player(CardinalDirection::West),
+                KeyboardInput::Right => game_state.maybe_move_player(CardinalDirection::East),
+                KeyboardInput::Up => game_state.maybe_move_player(CardinalDirection::North),
+                KeyboardInput::Down => game_state.maybe_move_player(CardinalDirection::South),
+                _ => (),
+            },
+            _ => (),
+        }
+    }
+}
+
+
+
+struct AppView {}
+
+impl AppView {
+    fn new() -> Self {
+        Self {}
+    }
+}
+
+impl <'a> View<&'a AppData> for AppView {
+    fn view<F: Frame, C: ColModify>(
+        &mut self,
+        data: &'a AppData,
+        context: ViewContext<C>,
+        frame: &mut F,
+    ) {
+        let view_cell = ViewCell::new()
+            .with_character('@')
+            .with_foreground(Rgb24::new_grey(255));
+        frame.set_cell_relative(data.game_state.player_coord, 0, view_cell, context);
+    }
+}
+
+
+
+pub struct App {
+    data: AppData,
+    view: AppView,
+}
+
+impl App {
+    fn new(screen_size: Size) -> Self {
+        Self {
+            data: AppData::new(screen_size),
+            view: AppView::new(),
+        }
+    }
+}
+
+impl CharGridApp for App {
+    fn on_input (
+        &mut self,
+        input: chargrid::app::Input,
+    ) -> Option<ControlFlow> {
+        match input {
+            Input::Keyboard(keys::ETX) | Input::Keyboard(keys::ESCAPE) => {
+                Some(chargrid::app::ControlFlow::Exit)
+            }
+            other => {
+                self.data.handle_input(other);
+                None
+            }
+        }
+    }
+
+    fn on_frame<F, C> (
+        &mut self,
+        _since_last_frame: Duration,
+        view_context: ViewContext<C>,
+        frame: &mut F,
+    ) -> Option<chargrid::app::ControlFlow>
+    where
+        F: Frame,
+        C: ColModify,
+    {
+        self.view.view(&self.data, view_context, frame);
+        None
+    }
+}
+
